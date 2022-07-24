611fc7cc87118dbcf2cee87af76454630de5e4a5
Michael Kroes <michael@kroes.email>
2022-07-24 10:18:17 +0200

    (Removed isDev identity not set notification)

--- app/src/main/java/info/nightscout/androidaps/MainApp.kt
+++ app/src/main/java/info/nightscout/androidaps/MainApp.kt
@@ -116,8 +116,8 @@ class MainApp : DaggerApplication() {
         handler.postDelayed(
             {
                 // check if identification is set
-                if (config.isDev() && sp.getStringOrNull(info.nightscout.core.utils.R.string.key_email_for_crash_report, null).isNullOrBlank())
-                    notificationStore.add(Notification(Notification.IDENTIFICATION_NOT_SET, rh.get().gs(R.string.identification_not_set), Notification.INFO))
+                //if (config.isDev() && sp.getStringOrNull(info.nightscout.core.utils.R.string.key_email_for_crash_report, null).isNullOrBlank())
+                //    notificationStore.add(Notification(Notification.IDENTIFICATION_NOT_SET, rh.get().gs(R.string.identification_not_set), Notification.INFO))
                 // log version
                 disposable += repository.runTransaction(VersionChangeTransaction(BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE, gitRemote, commitHash)).subscribe()
                 // log app start
